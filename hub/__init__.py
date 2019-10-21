aab9c31edace1acaf60018112c9ff6fded214e07
Sebastien Lelong <slelong@scripps.edu>
2019-10-21 08:58:17 -0700

    standaline api endpoints

--- hub/__init__.py
+++ hub/__init__.py
@@ -20,7 +20,18 @@ class AutoHubServer(HubServer):
 
     def __init__(self, version_urls, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.version_urls = version_urls 
+        self.version_urls = self.extract(version_urls)
+
+    def extract(self,urls):
+        vurls = []
+        for url in urls:
+            if isinstance(url,dict):
+                assert "name" in url and "url" in url
+                vurls.append(url)
+            else:
+                vurls.append({"name" : self.get_folder_name(url), "url" : url})
+
+        return vurls
 
     def cycle_update(self, src_name, version="latest", max_cycles=10):
         """
@@ -70,22 +81,25 @@ class AutoHubServer(HubServer):
         return folder.replace(".","_").replace("-","_")
 
     def list_biothings(self):
-        return [self.get_folder_name(url) for url in self.version_urls]
+        return self.version_urls
 
     def configure_autohub_feature(self):
         # "autohub" feature is based on "dump","upload" and "sync" features.
         # we don't list them in DEFAULT_FEATURES as we don't want them to produce
         # commands such as dump() or upload() as these are renamed for clarity
-        self.configure_dump_manager()
-        self.configure_upload_manager()
-        self.configure_sync_manager()
-        for version_url in self.version_urls:
+        # that said, those managers could still exist *if* autohub is mixed
+        # with "standard" hub, so we don't want to override them if already configured
+        if not self.managers.get("dump_manager"): self.configure_dump_manager()
+        if not self.managers.get("upload_manager"): self.configure_upload_manager()
+        if not self.managers.get("sync_manager"): self.configure_sync_manager()
+        for info in self.version_urls:
+            version_url = info["url"]
             self.__class__.DEFAULT_DUMPER_CLASS.VERSION_URL = version_url
             pidxr = partial(ESIndexer,index=btconfig.ES_INDEX_NAME,
                             doc_type=btconfig.ES_DOC_TYPE,es_host=btconfig.ES_HOST)
             partial_backend = partial(DocESBackend,pidxr)
         
-            SRC_NAME = self.get_folder_name(version_url)
+            SRC_NAME = info["name"]
             dump_class_name = "%sDumper" % self.get_class_name(SRC_NAME)
             # dumper
             dumper_klass = type(dump_class_name,(self.__class__.DEFAULT_DUMPER_CLASS,),
@@ -98,7 +112,6 @@ class AutoHubServer(HubServer):
                      })
             sys.modules["standalone.hub"].__dict__[dump_class_name] = dumper_klass
             self.managers["dump_manager"].register_classes([dumper_klass])
-
             # uploader
             # syncer will work on index used in web part
             esb = (btconfig.ES_HOST, btconfig.ES_INDEX_NAME, btconfig.ES_DOC_TYPE)
@@ -115,15 +128,30 @@ class AutoHubServer(HubServer):
             sys.modules["standalone.hub"].__dict__[uploader_class_name] = uploader_klass
             self.managers["upload_manager"].register_classes([uploader_klass])
 
+    def configure_commands(self):
+        super().configure_commands()
+        self.commands["list"] = CommandDefinition(command=self.list_biothings,tracked=False)
         # dump commands
         self.commands["versions"] = partial(self.managers["dump_manager"].call,method_name="versions")
         self.commands["check"] = partial(self.managers["dump_manager"].dump_src,check_only=True)
         self.commands["info"] = partial(self.managers["dump_manager"].call,method_name="info")
         self.commands["download"] = partial(self.managers["dump_manager"].dump_src)
-
         # upload commands
         self.commands["apply"] = partial(self.managers["upload_manager"].upload_src)
         self.commands["update"] = partial(self.cycle_update)
-        
-        self.commands["list"] = CommandDefinition(command=self.list_biothings,tracked=False)
+        self.commands["backend"] = partial(self.managers["dump_manager"].call,method_name="get_target_backend")
+
+    def configure_api_endpoints(self):
+        super().configure_api_endpoints()
+        from biothings.hub.api import EndpointDefinition
+        cmdnames = list(self.commands.keys())
+        self.api_endpoints["standalone"] = []
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="list",method="get",suffix="list"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="versions",method="get",suffix="versions"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="check",method="get",suffix="check"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="info",method="get",suffix="info"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="backend",method="get",suffix="backend"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="download",method="post",suffix="download"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="apply",method="post",suffix="apply"))
+        self.api_endpoints["standalone"].append(EndpointDefinition(name="update",method="post",suffix="update"))
 
